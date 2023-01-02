32e6ab2faa354f1923ddb16730941b2725331eb2
AbhishekGMV <abhishek.mvg@gmail.com>
2023-01-02 15:26:45 +0530

    Code restructure

---
+++ src/components/AddContact.js
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import axios from "axios";
+import TextField from "@mui/material/TextField";
+import { Button } from "@mui/material";
+
+export default function AddContact({ setContactLists, setOpen }) {
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState(false);
+  const userId = localStorage.getItem("id");
+  const addContact = async (e) => {
+    e.preventDefault();
+
+    const data = {
+      userId,
+      contact: {
+        username: userName,
+        email,
+      },
+    };
+    try {
+      const response = await axios.post(
+        "http://localhost:5500/contact/add",
+        data
+      );
+      if (response.status === 201) {
+        try {
+          setContactLists((prev) => [...prev, data]);
+        } catch (err) {
+          console.log(err);
+        }
+        setOpen(false);
+        return;
+      }
+    } catch (err) {
+      setError(true);
+      console.log(err);
+    }
+  };
+  return (
+    <form onSubmit={addContact}>
+      <TextField
+        id="outlined-multiline-flexible"
+        label="Name"
+        required
+        type="text"
+        style={{ margin: "1rem" }}
+        onChange={(e) => {
+          setUserName(e.target.value);
+        }}
+      />
+
+      <TextField
+        id="outlined-multiline-flexible"
+        label="Email"
+        required
+        type="email"
+        style={{ margin: "1rem" }}
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
+      {error ? (
+        <p style={{ color: "red", textAlign: "center" }}>
+          Error adding contact
+        </p>
+      ) : (
+        ""
+      )}
+      <Button
+        type="submit"
+        variant="outlined"
+        value="Submit"
+        style={{
+          margin: ".5rem 1rem",
+          backgroundColor: "#25a876",
+          color: "#fff",
+          padding: ".8rem 0",
+        }}
+      >
+        Submit
+      </Button>
+    </form>
+  );
+}
